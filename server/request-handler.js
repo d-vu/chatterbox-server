06831de1c3738ada84b124b3604ae69c54681717
d-vu <daniel.h.vu@gmail.com>
2016-05-09 16:58:53 -0700

    ✓ should respond to GET requests for /log with a 200 status code ✓ should send back parsable stringified JSON ✓ should send back an object ✓ should send an object containing a `results` array ✓ should accept POST requests to /send

--- server/request-handler.js
+++ server/request-handler.js
@@ -23,14 +23,26 @@ var requestHandler = function(request, response) {
   // http://nodejs.org/documentation/api/
 
   // Do some basic logging.
+
+  // The outgoing status.
+  var statusCode = 200;  
+
+
+
+
+
+
+
+  // POST REQUESTS
+  if (request.method === 'POST') {
+    statusCode = 201;
+  }
+
   //
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
 
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
@@ -39,11 +51,11 @@ var requestHandler = function(request, response) {
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  headers['Content-Type'] = 'application/json';
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  response.writeHead(statusCode, JSON.stringify(headers));
 
   // Make sure to always call response.end() - Node may not send
   // anything back to the client until you do. The string you pass to
@@ -52,7 +64,8 @@ var requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+
+  response.end(JSON.stringify({results: []}));
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
@@ -70,4 +83,6 @@ var defaultCorsHeaders = {
   'access-control-allow-headers': 'content-type, accept',
   'access-control-max-age': 10 // Seconds.
 };
+
+module.exports.requestHandler = requestHandler;
 
